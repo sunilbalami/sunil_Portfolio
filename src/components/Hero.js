d70805f11812c65df6602346ad3585c19c8d0d0a
sunilbalami <skstha77@gmail.com>
2023-03-13 18:52:31 +1100

    Landing page banner section complete

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,33 @@
 import React from "react";
-
+import suniB from "../assets/img/sunilB.svg";
 const Hero = () => {
   return (
-    <section className="lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden">
-      <div className="container mx-auto">Hero</div>
+    <section
+      id="home"
+      className="lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden"
+    >
+      <div className="container mx-auto h-full">
+        <div className="flex items-center h-full pt-8">
+          {/* left side*/}
+          <div className="flex-1  flex flex-col items-center lg:items-start ">
+            <p className=" text-lg text-accent mb=[22px]">Hey, I'm Sunil 🙋‍♂️</p>
+            <h1 className="text-2xl leading-[44px] md:text-3xl md:leading-tight  lg:text-5xl lg:leading-[1.2] font-bold md:tracking-[-2px] ">
+              I'm Front-End Developer
+            </h1>
+            <p className="pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left   ">
+              I'm Front end developer who writes and design clean, elegent and
+              efficint code.
+            </p>
+            <button className="btn btn-md bg-accent hover:bg-accent-hover md:btn-lg transition-all">
+              Hire me
+            </button>
+          </div>
+          {/* right side*/}
+          <div className="hidden lg:flex flex:1 justify-end items-end h-full">
+            <img src={suniB} alt="Background Image" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
